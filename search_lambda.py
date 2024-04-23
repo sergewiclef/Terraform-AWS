a1bebe181519118fe33602321b0f0fe1ff9a298e
sergewiclef <sergewiclef@gmail.com>
2024-04-23 15:52:32 +0200

    modified:   main.tf 	modified:   search.zip 	renamed:    twitter_search.py -> search_lambda.py

---
+++ search_lambda.py
@@ -0,0 +1,9 @@
+def search_lambda():
+    """Prints 'Hello, World!' to the console."""
+    print("Hello, World!")
+
+# Example usage:
+search_lambda()
+
+# End.
+
--- twitter_search.py
+++
@@ -1,9 +0,0 @@
-def search_lambda():
-    """Prints 'Hello, World!' to the console."""
-    print("Hello, World!")
-
-# Example usage:
-search_lambda()
-
-# End.
-
